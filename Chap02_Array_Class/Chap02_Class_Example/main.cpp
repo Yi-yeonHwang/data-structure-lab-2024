66e53d23f20f1efd5b6e26e17ddc7ce6a3356ca7
Yi-yeonHwang <yeon928@icloud.com>
2024-04-08 11:55:23 +0900

    Car

---
+++ Chap02_Array_Class/Chap02_Class_Example/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "Car.h"
+
+int main() {
+	Car myCar(50, "K3", 4);
+	Car yourCar(100, "K5", 3);
+	myCar.display();
+	yourCar.display();
+	myCar.whereAmI();
+	yourCar.whereAmI();
+
+}
